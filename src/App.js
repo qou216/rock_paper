39385e28af487a692999f901f6774975111cd0f3
=qou216 <qou216@naver.com>
2023-07-06 10:35:08 +0900

    button

--- src/App.js
+++ src/App.js
@@ -2,7 +2,24 @@
 import './App.css';
 import Box from './component/Box';
 
+const choice = { 
+  rock :{
+    name : "Rock",
+    img :"https://blog.kakaocdn.net/dn/bxgNpX/btqAHJcdNng/F9ZIdJ0idpvasVCXUsAqx0/img.png",
+  },
+  scissors : {
+    name : "Scissors",
+  },
+  paper:{
+    name : "Paper",
+    img:"https://t1.daumcdn.net/cfile/tistory/2509BA3F57358C8D09",
+  },
+}
+
 function App() {
+  const play=(userChoice)=>{
+    console.log("선택됌",userChoice);
+  }
   return (
     <div>
       <div className="main">
@@ -10,9 +27,9 @@ function App() {
        <Box title="Computer"/>
       </div>
       <div className="main">
-        <button>가위</button>
-        <button>바위</button>
-        <button>보</button>
+        <button onClick={()=>play("scissors")}>가위</button>
+        <button onClick={()=>play("rock")}>바위</button>
+        <button onClick={()=>play("paper")}>보</button>
       </div>
     </div>
   );
